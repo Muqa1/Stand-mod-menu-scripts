8c117ef2fccacb8ff492fccff2ef129f7da43d2e
Margus Kuusk <106272829+Muqa1@users.noreply.github.com>
2024-01-30 21:31:09 +0200

    Update Muqa ESP.lua

--- Muqa ESP.lua
+++ Muqa ESP.lua
@@ -23,16 +23,16 @@ local components = { -- name is components but its actually my overall cfg
     healtText = true,
     weaponName = true,
     distance = true,
-    maxDistance = 6000;
+    maxDistance = 10000,
+    sRd = 300, -- simplified rendering distance
 }
 local colors = {
     box = {r = 1.0, g = 1.0, b = 1.0, a = 1.0},
-    line = {r = 1.0, g = 1.0, b = 1.0, a = 0.2},
+    line = {r = 1.0, g = 1.0, b = 1.0, a = 1.0},
     name = {r = 0.0, g = 1.0, b = 1.0, a = 1.0},
     weapon = {r = 1.0, g = 1.0, b = 0.0, a = 1.0},
     distance = {r = 0.5, g = 0.5, b = 0.5, a = 1.0},
 }
-
 local bones = {
     leftFoot = 14201,
     rightFoot = 52301,
@@ -50,13 +50,6 @@ local bones = {
 --== tables ==--
 
 --== functions ==--
-local function DrawOutlinedRect(x,y,x1,y1,color)
-    directx.draw_line(x, y, x1, y, color) -- top
-    directx.draw_line(x, y, x, y1, color) -- left
-    directx.draw_line(x1, y, x1, y1, color) -- right
-    directx.draw_line(x, y1, x1, y1, color) -- bottom
-end
-
 local function worldToScreen(coords)
     local sx = memory.alloc()
     local sy = memory.alloc()
@@ -83,102 +76,127 @@ end
 
 --== script loop ==--
 menu.toggle_loop(menuroot, "Enable ESP", {"muqaesp"}, "Enables the ESP", function ()
-    local playerlist = players.list(components.localplayer, true, true)
-    local lPlayer = PLAYER.PLAYER_PED_ID()
+    local playerlist = players.list_except(not components.localplayer)
+    local lPlayer = players.user()
     for i = 1, #playerlist do
 
         local targetped = PLAYER.GET_PLAYER_PED(playerlist[i])
-        local pid = NETWORK.NETWORK_GET_PLAYER_INDEX_FROM_PED(targetped)
-
-        local dist = math.floor(v3.distance(ENTITY.GET_ENTITY_COORDS(lPlayer), ENTITY.GET_ENTITY_COORDS(targetped)))
-
+        local dist = math.floor(v3.distance(players.get_position(lPlayer), players.get_position(playerlist[i])))
         local health = ENTITY.GET_ENTITY_HEALTH(targetped)-100
 
         if (health <= 0) or (dist > components.maxDistance) then
             goto continue
         end
 
-        local head = PED.GET_PED_BONE_COORDS(targetped, bones.head, 0, 0, 0); head.z = head.z + 0.2
-        local w2sHead = worldToScreen(head)
+        if not (dist > components.sRd) then 
+            local head = PED.GET_PED_BONE_COORDS(targetped, bones.head, 0, 0, 0); head.z = head.z + 0.2
+            local w2sHead = worldToScreen(head)
 
-        local w2sPelvis = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.pelvis, 0, 0, 0))
+            local w2sPelvis = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.pelvis, 0, 0, 0))
 
-        local leftFoot = PED.GET_PED_BONE_COORDS(targetped, bones.leftFoot, 0, 0, 0); leftFoot.z = leftFoot.z - 0.1
-        local w2sLfoot = worldToScreen(leftFoot)
-        local rightFoot = PED.GET_PED_BONE_COORDS(targetped, bones.rightFoot, 0, 0, 0); rightFoot.z = rightFoot.z - 0.1
-        local w2sRfoot = worldToScreen(rightFoot)
+            local leftFoot = PED.GET_PED_BONE_COORDS(targetped, bones.leftFoot, 0, 0, 0); leftFoot.z = leftFoot.z - 0.1
+            local w2sLfoot = worldToScreen(leftFoot)
+            local rightFoot = PED.GET_PED_BONE_COORDS(targetped, bones.rightFoot, 0, 0, 0); rightFoot.z = rightFoot.z - 0.1
+            local w2sRfoot = worldToScreen(rightFoot)
 
-        local w2sRhand = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.rightHand, 0, 0, 0))
-        local w2sLhand = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.leftHand, 0, 0, 0))
+            local w2sRhand = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.rightHand, 0, 0, 0))
+            local w2sLhand = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.leftHand, 0, 0, 0))
 
-        local w2sRforearm = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.rightForearm, 0, 0, 0))
-        local w2sLforearm = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.leftForearm, 0, 0, 0))
+            local w2sRforearm = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.rightForearm, 0, 0, 0))
+            local w2sLforearm = worldToScreen(PED.GET_PED_BONE_COORDS(targetped, bones.leftForearm, 0, 0, 0))
 
-        --------------------------
-        if w2sHead.success and w2sLfoot.success and w2sRfoot.success then -- only draw if the player is within our screen
+            --------------------------
+            if w2sHead.success and w2sLfoot.success and w2sRfoot.success then -- only draw if the player is within our screen
 
-            local x_max = math.max(w2sHead.x, w2sPelvis.x, w2sLfoot.x, w2sRfoot.x, w2sRhand.x, w2sLhand.x, w2sRforearm.x, w2sLforearm.x)
-            local x_min = math.min(w2sHead.x, w2sPelvis.x, w2sLfoot.x, w2sRfoot.x, w2sRhand.x, w2sLhand.x, w2sRforearm.x, w2sLforearm.x)
-            local y_max = math.max(w2sHead.y, w2sPelvis.y, w2sLfoot.y, w2sRfoot.y, w2sRhand.y, w2sLhand.y, w2sRforearm.y, w2sLforearm.y)
-            local y_min = math.min(w2sHead.y, w2sPelvis.y, w2sLfoot.y, w2sRfoot.y, w2sRhand.y, w2sLhand.y, w2sRforearm.y, w2sLforearm.y)
-            
-            local x = x_min
-            local y = y_min
-            local w = x_max - x_min
-            local h = y_max - y_min
+                local x_max = math.max(w2sHead.x, w2sPelvis.x, w2sLfoot.x, w2sRfoot.x, w2sRhand.x, w2sLhand.x, w2sRforearm.x, w2sLforearm.x)
+                local x_min = math.min(w2sHead.x, w2sPelvis.x, w2sLfoot.x, w2sRfoot.x, w2sRhand.x, w2sLhand.x, w2sRforearm.x, w2sLforearm.x)
+                local y_max = math.max(w2sHead.y, w2sPelvis.y, w2sLfoot.y, w2sRfoot.y, w2sRhand.y, w2sLhand.y, w2sRforearm.y, w2sLforearm.y)
+                local y_min = math.min(w2sHead.y, w2sPelvis.y, w2sLfoot.y, w2sRfoot.y, w2sRhand.y, w2sLhand.y, w2sRforearm.y, w2sLforearm.y)
 
-            local maxhealth = ENTITY.GET_ENTITY_MAX_HEALTH(targetped)-100
+                local x, y, w, h = x_min, y_min, x_max - x_min, y_max - y_min
+
+                local maxhealth = ENTITY.GET_ENTITY_MAX_HEALTH(targetped)-100
+                local top_offset, bottom_offset = 0.0, 0.0
     
-            local color = {r = 1.0, g = 0.0, b = 1.0, a = 1.0}
-    
-            if components.box then
-                DrawOutlinedRect( x, y, x + w, y + h, colors.box)
-            end
-    
-            if components.line then 
-                directx.draw_line( 0.5, 1.0, x+w*0.5, y+h, colors.line)
-            end
-
-            if components.healthbar then 
-                local ratio = health / maxhealth
-                directx.draw_rect( x-0.002, y, -0.002, h, {r = 1.0 - (1.0 * ratio), g = (1.0 * ratio), b = 0.0, a = 1.0})
-                directx.draw_rect( x-0.002, y, -0.002, h - (h * (health / maxhealth)), {r = 0.0, g = 0.0, b = 0.0, a = 1.0})
-            end
-
-            if components.healthText then
-                directx.draw_text(x-0.006, y, math.floor((health / maxhealth) * 100), ALIGN_TOP_RIGHT, components.txtscale, {r = 0.0, g = 1.0, b = 0.0, a = 1.0}, true)
-            end
-
-            local top_offset = 0.0
-
-            if components.name then 
-                local name = NETWORK.NETWORK_PLAYER_GET_NAME(playerlist[i])
-                directx.draw_text(x+w*0.5, y - 0.002, name, ALIGN_BOTTOM_CENTRE, components.txtscale, colors.name, true)
-                local width, height = directx.get_text_size(name, components.txtscale)
-                top_offset = top_offset + height
-            end
-
-            if components.tags then 
-                if players.exists(pid) then 
-                    directx.draw_text(x+w*0.5, y-top_offset - 0.002, players.get_tags_string(pid), ALIGN_BOTTOM_CENTRE, components.txtscale, colors.name, true)
+                if components.box then
+                    directx.draw_line(x, y, x + w, y, colors.box) -- top
+                    directx.draw_line(x, y, x, y + h, colors.box) -- left
+                    directx.draw_line(x + w, y, x + w, y + h, colors.box) -- right
+                    directx.draw_line(x, y + h, x + w, y + h, colors.box) -- bottom
                 end
-            end
+    
+                if components.line then 
+                    directx.draw_line( 0.5, 1.0, x+w*0.5, y+h, colors.line)
+                end
 
-            local bottom_offset = 0.0
+                if components.healthbar then 
+                    local ratio = health / maxhealth
+                    directx.draw_rect( x-0.002, y, -0.002, h, {r = 1.0 - (1.0 * ratio), g = (1.0 * ratio), b = 0.0, a = 1.0})
+                    directx.draw_rect( x-0.002, y, -0.002, h - (h * (health / maxhealth)), {r = 0.0, g = 0.0, b = 0.0, a = 1.0})
+                end
 
-            if components.weaponName then 
-                local name = getWeaponName(targetped)
-                if name then
-                    directx.draw_text(x+w*0.5, y+h+0.002, name, ALIGN_TOP_CENTRE, components.txtscale, colors.weapon, true)
+                if components.healthText then
+                    directx.draw_text(x-0.006, y, math.floor((health / maxhealth) * 100), ALIGN_TOP_RIGHT, components.txtscale, {r = 0.0, g = 1.0, b = 0.0, a = 1.0}, true)
+                end
+
+                if components.name then 
+                    local name = players.get_name(playerlist[i])
+                    directx.draw_text(x+w*0.5, y - 0.002, name, ALIGN_BOTTOM_CENTRE, components.txtscale, colors.name, true)
                     local width, height = directx.get_text_size(name, components.txtscale)
-                    bottom_offset = bottom_offset + height
+                    top_offset = top_offset + height
+                end
+
+                if components.tags then 
+                    directx.draw_text(x+w*0.5, y-top_offset - 0.002, players.get_tags_string(playerlist[i]), ALIGN_BOTTOM_CENTRE, components.txtscale, colors.name, true)
+                end
+
+                if components.weaponName then 
+                    local name = getWeaponName(targetped)
+                    if name then
+                        directx.draw_text(x+w*0.5, y+h+0.002, name, ALIGN_TOP_CENTRE, components.txtscale, colors.weapon, true)
+                        local width, height = directx.get_text_size(name, components.txtscale)
+                        bottom_offset = bottom_offset + height
+                    end
+                end
+
+                if components.distance then 
+                    directx.draw_text(x+w*0.5, y+h+bottom_offset+0.002, dist.. "m", ALIGN_TOP_CENTRE, components.txtscale, colors.distance, true)
                 end
             end
+        else 
+            local w2sPos = worldToScreen(players.get_position(playerlist[i]))
+            if w2sPos.success then 
+                local top_offset, bottom_offset = 0.0, 0.0
+                local x, y = w2sPos.x, w2sPos.y
+            
+                if components.name then 
+                    local name = players.get_name(playerlist[i])
+                    directx.draw_text(x, y, name, ALIGN_BOTTOM_CENTRE, components.txtscale, colors.name, true)
+                    local width, height = directx.get_text_size(name, components.txtscale)
+                    top_offset = top_offset + height
+                end
 
-            if components.distance then 
-                directx.draw_text(x+w*0.5, y+h+bottom_offset+0.002, dist.. "m", ALIGN_TOP_CENTRE, components.txtscale, colors.distance, true)
+                if components.tags then 
+                    directx.draw_text(x, y - top_offset - 0.002, players.get_tags_string(playerlist[i]), ALIGN_BOTTOM_CENTRE, components.txtscale, colors.name, true)
+                end
+
+                if components.weaponName then 
+                    local name = getWeaponName(targetped)
+                    if name then
+                        directx.draw_text(x, y+0.002, name, ALIGN_TOP_CENTRE, components.txtscale, colors.weapon, true)
+                        local width, height = directx.get_text_size(name, components.txtscale)
+                        bottom_offset = bottom_offset + height
+                    end
+                end
+
+                if components.distance then 
+                    directx.draw_text(x, y+bottom_offset+0.002, dist.. "m", ALIGN_TOP_CENTRE, components.txtscale, colors.distance, true)
+                end
+
+                if components.line then 
+                    directx.draw_line( 0.5, 1.0, x, y, colors.line)
+                end
             end
-
         end
         ::continue::
     end
@@ -232,6 +250,10 @@ end, components.localplayer)
 
 menu.slider(espSetts, "Max Distance", {"muqaespboxmaxdst"}, "The maximum distance at which the player will be rendered.", 0, 10000, components.maxDistance, 1, function(value)
     components.maxDistance = value
+end)
+
+menu.slider(espSetts, "Simplified Rendering Distance", {"muqaespboxmaxsrd"}, "Past this distance the player bounding box wont be calculated since u cant see the box anyway.", 0, 10000, components.sRd, 1, function(value)
+    components.sRd = value
 end)
 
 menu.slider(espSetts, "Text Scale", {"muqaespboxtxtscale"}, "Change the ESP text size.", 0, 100, components.txtscale * 100, 1, function(value)
